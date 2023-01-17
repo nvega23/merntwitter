77a0051b5f1548fe2068115a0363b2cf67adcfcc
Nestor Vega <vega.nestor1@gmail.com>
2023-01-17 09:03:01 -0800

    validations

---
+++ backend/app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const { isProduction } = require('./config/keys');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+require('./models/User');
+require('./models/Tweet');
+require('./config/passport');
+const passport = require('passport')
+
+const User = mongoose.model('User');
+const Tweet = mongoose.model('Tweet');
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express
+    // server will serve the React files statically.)
+    app.use(cors());
+  }
+
+
+const csurf = require('csurf');
+// ...
+app.use(
+csurf({
+    cookie: {
+    secure: isProduction,
+    sameSite: isProduction && "Lax",
+    httpOnly: true
+    }
+})
+);
+
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+  });
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+serverErrorLogger(err);
+const statusCode = err.statusCode || 500;
+res.status(statusCode);
+res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors
+})
+});
+
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter)
+
+module.exports = app;
